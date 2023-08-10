24ea9f8c7dfb88e5fdb9f3143d0e2cb7b64ee1af
小傅哥 <184172133@qq.com>
2023-08-11 07:52:09 +0800

    feat: 授权验证

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,4 +1,14 @@
 const host = 'https://console-mock.apipost.cn/mock/072fa474-ab36-4650-a798-a57e8223e6e6'
+import { useAccessStore } from "@/app/store/access";
+
+function getHeaders() {
+    const accessState = useAccessStore.getState()
+    const headers =  {
+        Authorization:  accessState.token,
+        'Content-Type': 'application/json;charset=utf-8'
+    }
+    return headers
+}
 
 export const getRoleList = () => {
     // 从 apiPost mock 接口获取
