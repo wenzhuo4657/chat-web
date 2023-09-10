bfd0f2b6a9e723d411413acce621f6a9c0de1b55
小傅哥 <184172133@qq.com>
2023-09-10 13:35:17 +0800

    feat: dev-ops 第7节：网站添加百度统计

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,37 @@
 import "./styles/globals.scss";
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'ChatGPT - 小傅哥',
-  description: '您的 ChatGPT 贴心助手！',
+    title: 'ChatGPT - 小傅哥',
+    description: '您的 ChatGPT 贴心助手！',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              var _hmt = _hmt || [];
+              (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?8c8d73183929fe9e009a014e6f035f12";
+                var s = document.getElementsByTagName("script")[0]; 
+                s.parentNode.insertBefore(hm, s);
+              })();
+            `,
+                }}
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
