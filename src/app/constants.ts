5e0ff53ccc27000735ccb8c125d4a0df111100f1
小傅哥 <184172133@qq.com>
2023-10-15 16:57:01 +0800

    feat: 对接 chatglm 大模型

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -13,4 +13,10 @@ export enum GptVersion {
     GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k",
     GPT_4 = "gpt-4",
     GPT_4_32K = "gpt-4-32k",
+    CHATGLM_6B_SSE = "chatGLM_6b_SSE",
+    CHATGLM_LITE = "chatglm_lite",
+    CHATGLM_LITE_32K = "chatglm_lite_32k",
+    CHATGLM_STD = "chatglm_std",
+    CHATGLM_PRO = "chatglm_pro",
+
 }
